71742b6715350c28b37198e136a0340441dd4dfc
TehStud <igortadic.msc@gmail.com>
2022-09-21 13:12:23 +0200

    2

--- mycrafmenu.py
+++ mycrafmenu.py
@@ -1,4 +1,14 @@
 from ursina import *
 
+# BG = background
+class BG(Entity):
+	def __init__(self):
+		super().__init__(
+			parent = camera.ui,
+			model = 'quad',
+			scale = (0.56,0.86),
+			color = color.gray)
+
 app = Ursina()
+bg = BG()
 app.run()
