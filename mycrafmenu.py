140945d615801645e7b338d872b456659eb51798
TehStud <igortadic.msc@gmail.com>
2022-09-23 20:21:52 +0200

    Adding a draggable item object

--- mycrafmenu.py
+++ mycrafmenu.py
@@ -9,6 +9,15 @@ class BG(Entity):
 			scale = (0.56,0.86),
 			color = color.gray)
 
+class Item(Draggable):
+	def __init__(self):
+		super().__init__(
+			parent = camera.ui,
+			model = 'quad', 
+			color = color.red,
+			scale = (0.1, 0.1))
+
 app = Ursina()
 bg = BG()
+item = Item()
 app.run()
